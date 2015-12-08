8956c494847e5f1f56e39dfce1ea41a83d2bf6a2
Ken Kelly <ken@kenhkelly.us>
2015-12-08 16:12:23 -0500

    Add html templates and auto refreshing to more nicely display the command results

--- monitor.go
+++ monitor.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 	"os/exec"
 )
 
-func main() {
+type Page struct {
+	Title string
+	Body  string
+	Type  string
+}
 
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", serveHTTP)
 	http.HandleFunc("/ls", cmdLS)
 	http.HandleFunc("/vmstat", cmdVmstat)
@@ -14,68 +22,65 @@ func main() {
 	http.HandleFunc("/top", cmdTop)
 	http.HandleFunc("/iostat", cmdIostat)
 	http.ListenAndServe(":8080", nil)
+}
 
+func serveTemplate(d http.ResponseWriter, page *Page) {
+	d.Header().Add("Content Type", "text/html")
+	var file string
+	if page.Type == "home" {
+		file = "home"
+	} else {
+		file = "command"
+	}
+	tmpl, _ := template.ParseFiles("templates/home.html", "templates/command.html")
+	tmpl.ExecuteTemplate(d, file, page)
 }
 
 func serveHTTP(d http.ResponseWriter, req *http.Request) {
-	d.Header().Add("Content Type", "text/html")
-	out := `
-		To use a command include it in your url reques <br>
-		Example: http://localhost:8080/ls povides the output of the ls command <br>
-		<b>Supported Commands</b>: 
-		<ul>
-			<li><a href="/ls">ls</a></li>
-			<li><a href="/free">free</a></li>
-			<li><a href="/top">top</a></li>
-			<li><a href="/iostat">iostat</a></li>
-			<li><a href="/vmstat">vmstat</a></li>
-		</ul>
-	`
-	d.Write([]byte(out))
-
+	serveTemplate(d, &Page{Title: "Home", Body: "", Type: "home"})
 }
 
 func cmdLS(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("ls")
 	out, err := c1.Output()
-	d.Write(out)
 	if err != nil {
 		panic(err)
 	}
+	serveTemplate(d, &Page{Title: "Command: ls", Body: string(out), Type: "command"})
 }
 
 func cmdFree(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("free", "-h")
 	out, err := c1.Output()
-	d.Write(out)
 	if err != nil {
 		panic(err)
 	}
+	serveTemplate(d, &Page{Title: "Command: free", Body: string(out), Type: "command"})
 }
 
 func cmdTop(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("top", "-b", "-n 1")
 	out, err := c1.Output()
-	d.Write(out)
 	if err != nil {
 		panic(err)
 	}
+	serveTemplate(d, &Page{Title: "Command: top", Body: string(out), Type: "command"})
 }
 
 func cmdIostat(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("iostat")
 	out, err := c1.Output()
-	d.Write(out)
 	if err != nil {
-		panic(err)
+		out = []byte(`Command not available on this system`)
 	}
+	serveTemplate(d, &Page{Title: "Command: iostat", Body: string(out), Type: "command"})
 }
 
 func cmdVmstat(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("vmstat")
 	out, err := c1.Output()
-	d.Write(out)
 	if err != nil {
 		panic(err)
 	}
+	serveTemplate(d, &Page{Title: "Command: vmstat", Body: string(out), Type: "command"})
 }
