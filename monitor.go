2141e1cedfe2cf39a21953f17a6fb687287fdc5a
Ken Kelly <ken@kenhkelly.us>
2015-12-08 14:04:08 -0500

    Clean up home page and added quick links to the pages. Go format

--- monitor.go
+++ monitor.go
@@ -1,69 +1,74 @@
 package main
 
 import (
-  "net/http"
-  "os/exec"
+	"net/http"
+	"os/exec"
 )
 
-func main(){
+func main() {
 
-	http.HandleFunc("/",serveHTTP)
- 	http.HandleFunc("/ls", cmdLS)
+	http.HandleFunc("/", serveHTTP)
+	http.HandleFunc("/ls", cmdLS)
 	http.HandleFunc("/vmstat", cmdVmstat)
 	http.HandleFunc("/free", cmdFree)
 	http.HandleFunc("/top", cmdTop)
 	http.HandleFunc("/iostat", cmdIostat)
-  	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8080", nil)
 
 }
 
-func serveHTTP(d http.ResponseWriter,req *http.Request){
+func serveHTTP(d http.ResponseWriter, req *http.Request) {
 	d.Header().Add("Content Type", "text/html")
-	d.Write([]byte("To use a command include it in your url reques </br> Example http://localhost:8080/ls povides the output of the ls command </br> <b>Supported Commands</b> </br> ls free top iostat vmstat "))
+	out := `
+		To use a command include it in your url reques <br>
+		Example: http://localhost:8080/ls povides the output of the ls command <br>
+		<b>Supported Commands</b>: <a href="/ls">ls</a> <a href="/free">free</a> <a href="/top">top</a> <a href="/iostat">iostat</a> <a href="/vmstat">vmstat</a>
+	`
+	d.Write([]byte(out))
 
 }
 
-func cmdLS(d http.ResponseWriter,req *http.Request){
+func cmdLS(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("ls")
 	out, err := c1.Output()
 	d.Write(out)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 }
 
-func cmdFree(d http.ResponseWriter,req *http.Request){
+func cmdFree(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("free", "-h")
 	out, err := c1.Output()
 	d.Write(out)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 }
 
-func cmdTop(d http.ResponseWriter, req *http.Request){
+func cmdTop(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("top", "-b", "-n 1")
 	out, err := c1.Output()
 	d.Write(out)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 }
 
-func cmdIostat(d http.ResponseWriter, req *http.Request){
+func cmdIostat(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("iostat")
 	out, err := c1.Output()
 	d.Write(out)
-	if err != nil{
-	panic(err)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func cmdVmstat(d http.ResponseWriter, req *http.Request){
+func cmdVmstat(d http.ResponseWriter, req *http.Request) {
 	c1 := exec.Command("vmstat")
 	out, err := c1.Output()
 	d.Write(out)
-	if err != nil{
-	panic(err)
+	if err != nil {
+		panic(err)
 	}
 }
