11b5d644cd1a23618c3e960d3c612e49abae1615
user <user@linux-z5rr.CoxCommunications>
2016-05-03 11:52:36 -0500

    added man page lookup support. Text needs formatted to be readable.

--- monitor.go
+++ monitor.go
@@ -1,5 +1,6 @@
 package main
 
+//TODO today: add man page support on each command
 import (
 	"fmt"
 	"html/template"
@@ -21,6 +22,7 @@ func main() {
 	http.HandleFunc("/", serveHTTP)
 	http.HandleFunc("/ls", cmdLS)
 	http.HandleFunc("/ps", cmdPs)
+	http.HandleFunc("/man", man)
 	http.HandleFunc("/dmesg", cmdDmesg)
 	http.HandleFunc("/vmstat", cmdVmstat)
 	http.HandleFunc("/free", cmdFree)
@@ -29,15 +31,43 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+func man(d http.ResponseWriter, req *http.Request) {
+	var arg string
+	c1 := exec.Command("man", "man")
+	out, err := c1.Output()
+	if req.Method == "POST" {
+		req.ParseForm()
+		fmt.Println(req.Form["arg"])
+		if req.Form["arg"][0] != "" {
+			arg = ""
+			for i := 0; i < len(req.Form["arg"]); i++ {
+				arg += (req.Form["arg"][i])
+				strings.Replace(arg, "[", "", -1)
+				strings.Replace(arg, "]", "", -1)
+			}
+		}
+
+		fmt.Println("argument")
+		c1 = exec.Command("man", arg)
+		out, err = c1.Output()
+	}
+	if err != nil {
+		panicMyway(err, d)
+		return
+	}
+	serveTemplate(d, &Page{Title: "Manpages", Body: string(out), Type: "man"})
+}
 func serveTemplate(d http.ResponseWriter, page *Page) {
 	d.Header().Add("Content Type", "text/html")
 	var file string
 	if page.Type == "home" {
 		file = "home"
-	} else {
+	} else if page.Type=="command" {
 		file = "command"
+	} else if page.Type == "man" {
+		file = "man"
 	}
-	tmpl, _ := template.ParseFiles("templates/home.html", "templates/command.html")
+	tmpl, _ := template.ParseFiles("templates/home.html", "templates/command.html","templates/man.html")
 	tmpl.ExecuteTemplate(d, file, page)
 }
 
